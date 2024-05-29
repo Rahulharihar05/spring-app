f1645a6fccd8fafffbb8c6a152f25cc38c777371
Rahulharihar05 <hariharrs125@gmail.com>
2024-05-29 12:11:48 +0530

    first commit

---
+++ src/main/java/com/tssm/CalculatorServlet.java
@@ -0,0 +1,33 @@
+package com.tssm;
+
+
+/**
+ * Servlet implementation class CalculatorServlet
+ */
+public class CalculatorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public CalculatorServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
